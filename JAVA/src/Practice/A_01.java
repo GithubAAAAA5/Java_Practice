909fd9877c0ebd35e0ac45cc3c2d22d436f04c48
gusrh <gusrh@helleo>
2024-03-28 18:27:45 +0900

    240328

---
+++ JAVA/src/Practice/A_01.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class A_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		int b = 10;
+		System.out.println(a+b);
+	}
+
+}
