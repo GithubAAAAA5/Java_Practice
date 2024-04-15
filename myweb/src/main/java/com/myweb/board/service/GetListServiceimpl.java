b6e8d0c97e17a1e53ddbe308e4c9872cb61e035c
gusrh <gusrh@host.docker.internal>
2024-04-15 17:56:25 +0900

    240415 ce

---
+++ myweb/src/main/java/com/myweb/board/service/GetListServiceimpl.java
@@ -0,0 +1,14 @@
+package com.myweb.board.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetListServiceimpl implements IBoardService {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+	}
+
+}
